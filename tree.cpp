4ee74295661076cfcd815003ae4f6c832fbc1198
Mayamoho <119745370+Mayamoho@users.noreply.github.com>
2024-03-29 12:43:45 -0700

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -1,19 +1,20 @@
-//creation of binary tree using linked list and recursion
 #include<bits/stdc++.h>
 using namespace std;
 struct node{
     int data;
     node* left;
     node* right;
+    node(int x){
+        this.data = x;
+        left = right = NULL;
+    }
 };
+//tree creation
 node* tree(node* root){
      cout << "Enter data: " << '\n';
      int x;
      cin >> x;
-     root = new node();
-     root-> data = x;
-     root-> left = NULL;
-     root-> right = NULL;
+     root = new node(x);
      if(x==-1)
      return NULL;
      cout << "Enter data for left child of: " << x << '\n';
@@ -22,7 +23,46 @@ node* tree(node* root){
      root-> right = tree(root-> right);
      return root;
 }
+// inorder traversal
+void inorder(node* root){
+    if(!root)return;
+    inorder(root-> left);
+    cout << root-> data << ' ';
+    inorder(root-> right);
+}
+// preorder traversal
+void preorder(node* root){
+    if(!root)return;
+     cout << root-> data << ' ';
+     preorder(root-> left);
+     preorder(root-> right);
+}
+// postorder traversal
+void postorder(node* root){
+     if(!root)return;
+     postorder(root-> left);
+     postorder(root-> right);
+     cout << root-> data << ' ';
+}
+// finding an element in a tree
+bool find(node* root , int x){
+    if(!root) return false;
+    if(root-> data == x)return true;
+    return find(root-> left);
+    return find(root-> right);
+}
 int main(){
     node* root = NULL;
     root = tree(root);
+    inorder(root);
+    cout << '\n';
+    preorder(root);
+    cout << '\n';
+    postorder(root);
+    int n;
+    cin >> n;
+    if(find(root, n))
+    cout << "Yes\n";
+    else
+    cout << "No\n";
 }
