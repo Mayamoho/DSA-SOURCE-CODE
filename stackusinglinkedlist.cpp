4ef7beb035aa23c830117684bd795e0a74da7186
Mayamoho <119745370+Mayamoho@users.noreply.github.com>
2024-03-29 19:49:48 +0600

    Update stackusinglinkedlist.cpp

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -1,18 +1,19 @@
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 struct node{
     int data;
     node* link;
 };
-node* top = NULL;
+node* head = NULL;
 //checking if the stack is empty
 bool isEmpty(){
-    if(top==NULL)
+    if(head==NULL)
     return true;
     else
     return false;
 }
-//insert a new node at the top of stack
+//insert a new node at the head of stack
+// same as inserting new node at the beginning of a singly linked list
 void push(int data){
     //new can be used instead of calloc/malloc to allocate memory dynamically in C++/Java
     node* temp = new node();
@@ -20,25 +21,28 @@ void push(int data){
     temp-> link = NULL;
     if(isEmpty())
     {
-        top = temp;
+        head = temp;
         return;
     }
     else{
-        temp-> link = top;
-        top = temp;
+        temp-> link = head;
+        head = temp;
     }
 }
 //deleting the topmost node
+// same as deleting node at the beginning of a singly linked list
 void pop(){
-    if(isEmpty())
+    if(isEmpty()){
     cout << "Stack Underflow\n";
-    else if(top-> link == NULL){
-            free(top);
-            top = NULL;
+    return;
+    }
+    else if(head-> link == NULL){
+            free(head);
+            head = NULL;
     }
     else{
-        node* ptr = top;
-        top = top-> link;
+        node* ptr = head;
+        head = head-> link;
         free(ptr);
         ptr = NULL;  
     }
@@ -46,16 +50,16 @@ void pop(){
 //displaying the topmost element
 int peek(){
     if(isEmpty())
-    cout << "Stack Underflow\n";
+    return -1;
     else
-    return top-> data;
+    return head-> data;
 }
 void printdata(){
     if(isEmpty())
     cout << "Stack is empty\n";
     else{
-        node* ptr = top;
-        while(ptr!=NULL){
+        node* ptr = head;
+        while(ptr){
             cout << ptr-> data << ' ';
             ptr = ptr-> link;
         }
@@ -76,3 +80,110 @@ int main(){
     cout << peek() << '\n';
     cout << isEmpty() << '\n';
 }
+*/
+// infix to postfix conversion
+/*#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    char ch;
+    Node *next;
+};
+struct Stack
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    void push(char ch)
+    {
+        Node *temp = new Node();
+        temp->ch = ch;
+        if (head == NULL)
+        {
+            head = temp;
+            head->next = NULL;
+            tail = head;
+        }
+        else
+        {
+            temp->next = head;
+            head = temp;
+        }
+    }
+    char pop()
+    {
+        Node *temp = head;
+        head = head->next;
+        return temp->ch;
+    }
+};
+int val(char ch)
+{
+    if (ch == '^')
+        return 5;
+    if (ch == '/')
+        return 3;
+    if (ch == '*')
+        return 3;
+    if (ch == '+')
+        return 2;
+    if (ch == '-')
+        return 2;
+    return 0;
+}
+int main()
+{
+    Stack *output_stack = new Stack();
+    Stack *operator_stack = new Stack();
+    string str1;
+    cin >> str1;
+    for (int i = 0; i < str1.size(); i++)
+    {
+        if ((str1[i] >= 'A' && str1[i] <= 'Z') || (str1[i] >= 'a' && str1[i] <= 'z'))
+        {
+            output_stack->push(str1[i]);
+        }
+        else if (str1[i] == '(')
+        {
+            operator_stack->push(str1[i]);
+        }
+        else if (str1[i] == ')')
+        {
+            while (operator_stack->head != NULL && operator_stack->head->ch != '(')
+            {
+                char ch2 = operator_stack->pop();
+                output_stack->push(ch2);
+            }
+            operator_stack->pop();
+        }
+        else
+        {
+            if (operator_stack->head == NULL)
+            {
+                operator_stack->push(str1[i]);
+                continue;
+            }
+            int res1 = val(str1[i]);
+            while (operator_stack->head != NULL && (val(operator_stack->head->ch) >= res1 && operator_stack->head->ch != '(' && operator_stack->head->ch != ')'))
+            {
+                char ch2 = operator_stack->pop();
+                output_stack->push(ch2);
+            }
+            operator_stack->push(str1[i]);
+        }
+    }
+    while (operator_stack->head != NULL)
+    {
+        char ch2 = operator_stack->pop();
+        output_stack->push(ch2);
+    }
+    string str2;
+    while (output_stack->head != NULL)
+    {
+        str2.push_back(output_stack->pop());
+    }
+    reverse(str2.begin(), str2.end());
+    cout << str2;
+    cout << "\n";
+    return 0;
+}
+*/
